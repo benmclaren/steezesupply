621240fefd66314e141bf65e78d6937fd3b83d73
Ben McLaren <benmclaren@live.co.uk>
2025-03-27 15:02:44 +0000

    completed navbar

--- app/javascript/controllers/mobile_menu_controller.js
+++ app/javascript/controllers/mobile_menu_controller.js
@@ -2,13 +2,24 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="mobile-menu"
 export default class extends Controller {
-  static targets = ["nav"]
+  static targets = ["nav", "body"]
+
+  connect() {
+    console.log('connected');
+  }
 
   toggle(event) {
     event.stopPropagation();
     // console.log('clicked')
     console.log(this.navTarget)
     console.log(event)
-    this.navTarget.classList.toggle('open');
+    this.navTarget.classList.add('open');
+    this.bodyTarget.classList.add('shadow')
   }
+
+  close(event) {
+    this.navTarget.classList.remove('open')
+    this.bodyTarget.classList.remove('shadow')
+  }
+
 }
